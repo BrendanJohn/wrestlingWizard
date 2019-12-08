66003145c92c8ad1dd3c4e74f44e14167153f74e
Brendan Murphy <bjohnmurphy1984@gmail.com>
2019-12-08 04:03:15 +0000

    added procfile

--- application.py
+++ application.py
@@ -18,7 +18,12 @@ from helpers import apology, login_required, lookup, usd
 # CREATE TABLE IF NOT EXISTS 'users' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'username' TEXT NOT NULL, 'hash' TEXT NOT NULL, 'points' NUMERIC NOT NULL DEFAULT 10000.00 );
 # Configure application
 app = Flask(__name__)
-app.secret_key = "brendan"
+app.config['SECRET_KEY'] = config.SECRET_KEY
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
--- config.py
+++ config.py
@@ -3,8 +3,4 @@ from flask_session import Session
 from tempfile import mkdtemp
 
 app = Flask(__name__)
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+app.secret_key = "brendan"
