49bffb30c7d9f8cf3c0bfaca4e852c0488d0a2cd
Brendan Murphy <bjohnmurphy1984@gmail.com>
2019-12-08 03:56:39 +0000

    added procfile

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from flask_session import Session
 from tempfile import mkdtemp
 
 app = Flask(__name__)
-app.secret_key = "brendan"
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
