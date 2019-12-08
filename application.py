ce07b2347c9e5f739081fa1752262236155ccb7a
Brendan Murphy <bjohnmurphy1984@gmail.com>
2019-12-08 02:37:56 +0000

    added procfile

--- application.py
+++ application.py
@@ -37,6 +37,7 @@ app.jinja_env.filters["usd"] = usd
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config['SECRET_KEY'] = os.urandom(12)
 Session(app)
 
 # Configure CS50 Library to use SQLite database
