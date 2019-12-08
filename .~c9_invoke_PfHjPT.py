d179361d2801c2f4dfe2a69bf4ecc5da3600ca57
Brendan Murphy <bjohnmurphy1984@gmail.com>
2019-12-08 04:32:36 +0000

    added procfile

--- .~c9_invoke_PfHjPT.py
+++ .~c9_invoke_PfHjPT.py
@@ -19,14 +19,7 @@ from helpers import apology, login_required, lookup, usd
 # Configure application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config.SECRET_KEY
-# Configure session to use filesystem (instead of signed cookies)
-#app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-#app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
-# Ensure templates are auto-reloaded
-app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Ensure responses aren't cached
 @app.after_request
--- config.py
+++ config.py
@@ -4,3 +4,11 @@ from tempfile import mkdtemp
 
 app = Flask(__name__)
 SECRET_KEY = "brendan"
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
