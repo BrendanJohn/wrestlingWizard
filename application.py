e89d583e2ba7a16f583c4f04e7ca413960badbbc
Brendan Murphy <bjohnmurphy1984@gmail.com>
2019-12-08 17:55:24 +0000

    added procfile

--- application.py
+++ application.py
@@ -18,8 +18,6 @@ from helpers import apology, login_required, lookup, usd
 # Configure application
 app = Flask(__name__)
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMANENT"] = True
-app.config["SESSION_TYPE"] = "filesystem"
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 Session(app)
 # Ensure templates are auto-reloaded
